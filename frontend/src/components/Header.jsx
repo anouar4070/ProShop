19c07cacac94d799422f42d459072dc1526d3a7a
anouar garbaa <g.anouar@yahoo.com>
2025-04-27 17:26:44 +0200

    use Link instead of LinkContainer to fix  the problem of "React Router v6.4+ Integration with LinkContainer"

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "rect-router-bootstrap";
+//import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom"; // ✅ use Link directly instead of LinkContainer
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -9,31 +10,23 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="ProShop Logo" />
-              ProShop
-            </Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="ProShop Logo" style={{ height: '30px', marginRight: '10px' }} />
+            ProShop
+          </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
 
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <FaShoppingCart />
-                  Cart
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart /> Cart
+              </Nav.Link>
 
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser />
-                  Sign In
-                </Nav.Link>
-              </LinkContainer>
-              
+              <Nav.Link as={Link} to="/login">
+                <FaUser /> Sign In
+              </Nav.Link>
+
             </Nav>
           </Navbar.Collapse>
         </Container>
