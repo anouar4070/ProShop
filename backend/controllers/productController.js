484bdfb88cc0f7a92bacbc199383b9cf868c2606
anouar garbaa <g.anouar@yahoo.com>
2025-05-08 14:54:45 +0200

    Creating Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
-
 // @desc  Fetch all products
 // @route GET /api/products
 // @access Public
@@ -24,4 +23,24 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc  Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, createProduct };
