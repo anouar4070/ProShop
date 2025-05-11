55aeb4136491f0d76b625db9f9858d08dc474287
anouar garbaa <g.anouar@yahoo.com>
2025-05-11 05:31:52 +0200

    PayPal & Price Calculate Fixes (PayPal Token & Backend Calculation)

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -65,7 +65,7 @@ const OrderScreen = () => {
   function onApprove(data, actions) {
     return actions.order.capture().then(async function (details) {
       try {
-        await payOrder({ orderId, details });
+        await payOrder({ orderId, details }).unwrap();
         refetch();
         toast.success("Payment successful");
       } catch (err) {
@@ -112,7 +112,7 @@ const OrderScreen = () => {
   return isLoading ? (
     <Loader />
   ) : error ? (
-    <Message variant='danger'>{error?.data?.message || error.error}</Message>
+    <Message variant="danger">{error?.data?.message || error.error}</Message>
   ) : (
     // <Message variant="danger" />
     <>
